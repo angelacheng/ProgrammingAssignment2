4fbe086d66feb5ada1a0f87e61302cfd799dfda3
angelacheng <angelacmw1989@gmail.com>
2014-06-21 16:38:46 -0400

    Submitting assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Based on example R script, I have two functions written here. 
+## The purpose of the functions is to compute the inverseerse of a square matrix
+## For example, if X is a square inverseertible matrix, then solve(X) returns its inverseerse
 
-## Write a short comment describing this function
+
+## This function creates a special "matrix" object that can cache its inverseerse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  
+  get <- function() x
+  setinverse <- function(solve) inverse <<- solve
+  getinverse <- function() inverse
+  
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverseerse of the special "matrix" 
+## returned by makeCacheMatrix above. 
+## If the inverseerse has already been calculated (and the matrix has not changed)
+## then the cachesolve should retrieve the inverseerse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  inverse <- x$getinverse()
+  
+  ## check if inverse exists in cache, then return it
+  if(!is.null(inverse)) {
+    message("Cached data used")
+    return(inverse)
+  }
+  
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setinverse(inverse)
+  inverse
 }
